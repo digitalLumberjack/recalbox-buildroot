7068e386d656b5feef12c857e9a5735154bb9bc1
hstampfl <heinz.st@mpf.li>
2015-03-16 22:13:50 +0100

    add custom settings /recalbox/share/system/custom_recalbox.conf
    add system settings from /recalbox/share/system/custom_recalbox.conf when file is available and setting is implemented there. 
    if file does not exists it takes default value from /recalbox/share/system/recalbox.conf

--- board/recalbox/fsoverlay/recalbox/scripts/systemsetting.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/systemsetting.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
 system_settings=/recalbox/share/system/recalbox.conf
+custom_system_settings=/recalbox/share/system/custom_recalbox.conf
 
 command="$1"
 argsetting="$2"
 log=/root/recalbox.log
 
 if [[ "$command" == "get" ]];then
-	echo "`logtime` : systemsetting.sh - searching for $argsetting" >> $log
-	setting=`cat "$system_settings" | sed -n "s/^${argsetting}=\(.*\)/\1/p"`
+	echo "`logtime` : systemsetting.sh - searching for custom settings $argsetting" >> $log
+	setting=`cat "$custom_system_settings" | sed -n "s/^${argsetting}=\(.*\)/\1/p"`
 	if [[ "$?" != "0" ]]; then
+	        setting=`cat "$system_settings" | sed -n "s/^${argsetting}=\(.*\)/\1/p"`
+	        if [[ "$?" != "0" ]]; then
+	                exit 1
+	        fi
+	        if [[ "$setting" != ""]]; then
+	               echo "`logtime` : systemsetting.sh - default $argsetting found : $setting" >> $log
+		       echo $setting
+		       exit 0
+	        fi
 		exit 1
 	fi
 	if [[ "$setting" != "" ]]; then
-		echo "`logtime` : systemsetting.sh - $argsetting found : $setting" >> $log
+		echo "`logtime` : systemsetting.sh - custom $argsetting found : $setting" >> $log
 		echo $setting
 		exit 0
 	fi
 fi
 exit 1
-
