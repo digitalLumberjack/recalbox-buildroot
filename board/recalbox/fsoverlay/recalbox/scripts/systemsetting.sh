7c6ebd12652ebb05475e0fa73489696b6bee3180
hstampfl <heinz.st@mpf.li>
2015-03-16 23:24:19 +0100

    reimplemented logic, it should work now ;)

--- board/recalbox/fsoverlay/recalbox/scripts/systemsetting.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/systemsetting.sh
@@ -9,23 +9,21 @@ log=/root/recalbox.log
 
 if [[ "$command" == "get" ]];then
 	echo "`logtime` : systemsetting.sh - searching for settings $argsetting" >> $log
-	setting=`cat "$custom_system_settings" | sed -n "s/^${argsetting}=\(.*\)/\1/p"`
+	csetting=`cat "$custom_system_settings" | sed -n "s/^${argsetting}=\(.*\)/\1/p"`
 	if [[ "$?" != "0" ]]; then
+		exit 1
+	fi
+	if [[ "$csetting" != "" ]]; then
+		echo "`logtime` : systemsetting.sh - customsetting $argsetting found : $setting" >> $log
+		echo $csetting
+		exit 0
+	else
 	        setting=`cat "$system_settings" | sed -n "s/^${argsetting}=\(.*\)/\1/p"`
-	        if [[ "$?" != "0" ]]; then
-	                exit 1
-	        fi
 	        if [[ "$setting" != "" ]]; then
 	               echo "`logtime` : systemsetting.sh - defaultsetting $argsetting found : $setting" >> $log
 		       echo $setting
 		       exit 0
 	        fi
-		exit 1
-	fi
-	if [[ "$setting" != "" ]]; then
-		echo "`logtime` : systemsetting.sh - customsetting $argsetting found : $setting" >> $log
-		echo $setting
-		exit 0
 	fi
 fi
 exit 1
