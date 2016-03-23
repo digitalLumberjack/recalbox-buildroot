55a1f3b2d67f35a18a68c73baeab49086c59ef91
Yann Morère <yann.morere@univ-lorraine.fr>
2016-03-23 14:50:34 +0100

    adding dynamic subtitle creation in /tmp for intro video

--- board/recalbox/fsoverlay/recalbox/system/resources/splash/CreateSubT.sh
+++ board/recalbox/fsoverlay/recalbox/system/resources/splash/CreateSubT.sh
@@ -3,4 +3,4 @@ cat > /tmp/subtitle.srt << EOF
 1
 00:00:00,000 --> 00:00:01,000
 EOF
-cat >> /tmp/subtitle.srt < /recalbox/share/system/update.done
+cat >> /tmp/subtitle.srt < /recalbox/recalbox.version
