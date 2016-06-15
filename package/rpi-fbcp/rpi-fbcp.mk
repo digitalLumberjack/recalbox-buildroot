798dbf3a648b47379f120d91701151ffc7eb36cd
Yann Morère <yann.morere@univ-lorraine.fr>
2016-06-15 15:20:58 +0200

    Adding fbcp program for small TFT screen

--- package/rpi-fbcp/rpi-fbcp.mk
+++ package/rpi-fbcp/rpi-fbcp.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-RPI_FBCP_VERSION = c1d05f8d8e02641726691f95d9cd85a76a33af85
-RPI_FBCP_SITE = $(call github,ian57,rpi-fbcp,$(RPI_FBCP_VERSION))
-RPI_FBCP_DEPENDENCIES += rpi-userland
+RPI_FBCP_VERSION = 8087a71d0330a078d91aa78656684ab5313616c6
+RPI_FBCP_SITE = $(call github,tasanakorn,rpi-fbcp,$(RPI_FBCP_VERSION))
 
 define RPI_FBCP_CROSS_FIXUP
         $(SED) 's|/opt/vc/include|$(STAGING_DIR)/usr/include|g' $(@D)/CMakeLists.txt 
