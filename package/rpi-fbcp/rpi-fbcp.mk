ca6dc7f6a253723f03e4d4afc64857c1ae107b17
Yann Morère <yann.morere@univ-lorraine.fr>
2016-06-27 08:32:23 +0200

    back to sdl 2.0.4

--- package/rpi-fbcp/rpi-fbcp.mk
+++ package/rpi-fbcp/rpi-fbcp.mk
@@ -6,8 +6,8 @@
 
 RPI_FBCP_VERSION = 8087a71d0330a078d91aa78656684ab5313616c6
 RPI_FBCP_SITE = $(call github,tasanakorn,rpi-fbcp,$(RPI_FBCP_VERSION))
+RPI_FBCP_DEPENDENCIES += rpi-userland
 
-define RPI_FBCP_CROSS_FIXUP
         $(SED) 's|/opt/vc/include|$(STAGING_DIR)/usr/include|g' $(@D)/CMakeLists.txt 
         $(SED) 's|/opt/vc/lib|$(STAGING_DIR)/usr/lib|g' $(@D)/CMakeLists.txt 
         $(SED) 's|bcm_host|bcm_host vchostif|g' $(@D)/CMakeLists.txt 
