1eed2a828e5b593c0170f1fd2d9798579b30972c
Yann Morère <yann.morere@univ-lorraine.fr>
2016-06-27 08:36:03 +0200

    fix rpi-fbcp.mk

--- package/rpi-fbcp/rpi-fbcp.mk
+++ package/rpi-fbcp/rpi-fbcp.mk
@@ -8,6 +8,7 @@ RPI_FBCP_VERSION = c1d05f8d8e02641726691f95d9cd85a76a33af85
 RPI_FBCP_SITE = $(call github,ian57,rpi-fbcp,$(RPI_FBCP_VERSION))
 RPI_FBCP_DEPENDENCIES += rpi-userland
 
+define RPI_FBCP_CROSS_FIXUP
         $(SED) 's|/opt/vc/include|$(STAGING_DIR)/usr/include|g' $(@D)/CMakeLists.txt 
         $(SED) 's|/opt/vc/lib|$(STAGING_DIR)/usr/lib|g' $(@D)/CMakeLists.txt 
         $(SED) 's|bcm_host|bcm_host vchostif|g' $(@D)/CMakeLists.txt 
